2125553ac13a16a4873f0f12c49e67de31d3214d
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-26 00:07:41 +0000

    completed Exercise 2

--- exercise-#.js
+++ exercise-#.js
@@ -1,1 +1,37 @@
 //Exercise 1
+
+var mysql = require('mysql');
+var colors = require('colors');
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+// connection.query("SELECT * FROM Account", function(err, rows, fields) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//          rows.forEach(function(row) {
+//          console.log('#' + row.id + ': ' + row.email.cyan);
+//         });
+//     }
+// });
+
+// // connection.end();
+
+//Exercise 2
+
+connection.query("SELECT * FROM Account LIMIT 5", function(err, rows, fields) {
+    if (err) {
+        console.log(err);
+    } else {
+         rows.forEach(function(row) {
+         console.log('#' + row.id + ': ' + row.email.bold);
+        });
+    }
+});
+
+connection.end();
