eb04f99d7566cca746966b9ebe8773dc7c3a7d8e
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-26 21:52:55 +0000

    divided up exercise 2

---
+++ exercise-2.js
@@ -0,0 +1,23 @@
+//Exercise 2
+
+var mysql = require('mysql');
+var colors = require('colors');
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+connection.query("SELECT * FROM Account LIMIT 5", function(err, rows, fields) {
+    if (err) {
+        console.log(err);
+    } else {
+         rows.forEach(function(row) {
+         console.log('#' + row.id + ': ' + row.email.bold);
+        });
+    }
+});
+
+connection.end();
